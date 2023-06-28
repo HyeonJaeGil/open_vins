a39ee81fff16d1bc5c0e153a4edd642714d8c733
HyeonJaeGil <now9728@gmail.com>
2023-06-29 05:12:42 +0900

    remove ros-related part

---
+++ ov_core/cmake/non-ROS.cmake
@@ -0,0 +1,57 @@
+cmake_minimum_required(VERSION 3.3)
+
+add_definitions(-DROS_AVAILABLE=0)
+include(GNUInstallDirs)
+
+# Include our header files
+include_directories(
+        src
+        ${EIGEN3_INCLUDE_DIR}
+        ${Boost_INCLUDE_DIRS}
+        ${catkin_INCLUDE_DIRS}
+)
+
+# Set link libraries used by all binaries
+list(APPEND thirdparty_libraries
+        ${Boost_LIBRARIES}
+        ${OpenCV_LIBRARIES}
+        ${catkin_LIBRARIES}
+)
+
+##################################################
+# Make the core library
+##################################################
+
+list(APPEND LIBRARY_SOURCES
+        src/dummy.cpp
+        src/cpi/CpiV1.cpp
+        src/cpi/CpiV2.cpp
+        src/sim/BsplineSE3.cpp
+        src/track/TrackBase.cpp
+        src/track/TrackAruco.cpp
+        src/track/TrackDescriptor.cpp
+        src/track/TrackKLT.cpp
+        src/track/TrackSIM.cpp
+        src/types/Landmark.cpp
+        src/feat/Feature.cpp
+        src/feat/FeatureDatabase.cpp
+        src/feat/FeatureInitializer.cpp
+        src/utils/print.cpp
+)
+file(GLOB_RECURSE LIBRARY_HEADERS "src/*.h")
+add_library(ov_core_lib SHARED ${LIBRARY_SOURCES} ${LIBRARY_HEADERS})
+target_link_libraries(ov_core_lib ${thirdparty_libraries})
+target_include_directories(ov_core_lib
+    PUBLIC 
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+)
+install(TARGETS ov_core_lib
+        ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+        LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+        RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+)
+install(DIRECTORY src/
+        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/open_vins/
+        FILES_MATCHING PATTERN "*.h" PATTERN "*.hpp"
+)
